507c9e2c8878d01ac6114b653e4bd6e71246903f
jmercado <jmercado@propertyvista.com>
2023-04-14 13:33:08 +0800

    exclude swagger on security

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -2,26 +2,30 @@ package com.example.demo;
 
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter  {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	 @Override
-	    protected void configure(final HttpSecurity http) throws Exception {
-	        http.authorizeRequests()
-	                .antMatchers("/**").fullyAuthenticated()
-	                .and()
-	                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	                .and()
-	                .oauth2ResourceServer().jwt()
-	                .and()
-	                .and()
-	                .cors().and().csrf().disable();
-	    }
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/**").fullyAuthenticated().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().oauth2ResourceServer().jwt().and().and()
+				.cors().and().csrf().disable();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+	}
 
-    
 }
